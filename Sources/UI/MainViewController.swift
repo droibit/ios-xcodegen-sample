f421daf9c2b168e124599c5bc4df82e6c94b4753
Shinya Kumagai <roomful.rooms@gmail.com>
2020-02-16 16:12:07 +0900

    Setup minimum ios app

---
+++ Sources/UI/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  XcodeGenSample
+//
+//  Created by Shinya Kumagai on 2/16/20.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+         super.viewDidLoad()
+        print("TEST")
+    }
+}
